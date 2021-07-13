aff87fb7d7c4b4128e7b06888a17fa9b575e31ac
Ismael Dominguez <ismaeldom11@gmail.com>
2021-07-13 13:29:43 -0500

    Se crea vista para login
    Se crea el formulario para el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+@Injectable()
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email!: string;
+  password!: string;
+
+  constructor(private router: Router,
+              private flashMessages: FlashMessagesService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+
   }
 
 }
